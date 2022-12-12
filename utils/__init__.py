d790e75df6c90e12c1b174db805805abfab890f4
Jan Van Overwalle <janvanoverwalle@gmail.com>
2022-12-12 17:35:02 +0100

    Updated gitignore and added generic utils

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['read_input']
+
+from .input import read_input
---
+++ utils/input.py
@@ -0,0 +1,30 @@
+import os
+
+from typing import Union
+
+
+def __get_day_from_filename(filename):
+    path = os.path.splitext(filename)[0]
+    path = os.path.split(path)[-1]
+    day = path.strip().lower().replace('day', '')
+    return int(day)
+
+
+def read_input_by_day(day: int, split_lines=True):
+    with open(os.path.join('inputs', f'day{day:0>2}.txt')) as in_file:
+        data = in_file.read()
+        if split_lines:
+            data = data.splitlines()
+        return data
+
+
+def read_input_by_filename(filename: str, split_lines=True):
+    day = __get_day_from_filename(filename)
+    return read_input_by_day(day, split_lines=split_lines)
+
+
+def read_input(day_or_filename: Union[int, str], split_lines=True):
+    try:
+        return read_input_by_day(int(day_or_filename), split_lines=split_lines)
+    except ValueError:
+        return read_input_by_filename(day_or_filename, split_lines=split_lines)
