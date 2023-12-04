63aa5112e071254f685d952476a11c5372e5c6d0
Jan Van Overwalle <janvanoverwalle@gmail.com>
2023-12-04 19:02:40 +0100

    Added 2023 slution template

---
+++ 2023/_template_/solution.py
@@ -0,0 +1,24 @@
+"""https://adventofcode.com/2023/day/xx"""
+
+import sys
+from os.path import abspath, dirname
+
+sys.path.append(dirname(dirname(dirname(abspath(__file__)))))
+
+from utils import read_input
+
+
+def placeholder():
+    lines = read_input(__file__, example=True)
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+part1()
+part2()
