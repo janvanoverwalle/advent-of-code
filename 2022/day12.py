4d2b5f5d56111e24bc79f5401e776ac2ae175e8d
Jan Van Overwalle <janvanoverwalle@gmail.com>
2023-12-04 19:02:25 +0100

    Revamped project structure

---
+++ 2022/day12.py
@@ -0,0 +1,15 @@
+"""https://adventofcode.com/2022/day/12"""
+
+from utils import read_input, read_example_input
+
+
+def part1():
+    lines = read_example_input(__file__)
+
+
+def part2():
+    lines = read_example_input(__file__)
+
+
+part1()
+#part2()
--- utils/input.py
+++ utils/input.py
@@ -1,13 +1,22 @@
 import os
 
+from datetime import datetime
 from typing import Union
 
 
-def __get_day_from_filename(filename):
+def __get_day_from_filename_v1(filename):
     path = os.path.splitext(filename)[0]
     path = os.path.split(path)[-1]
     day = path.strip().lower().replace('day', '')
     return int(day)
+
+
+def __get_day_from_filename_v2(filename):
+    return int(os.path.split(os.path.dirname(os.path.normpath(filename)))[1])
+
+
+def __get_day_from_filename(filename):
+    return __get_day_from_filename_v2(filename)
 
 
 def __read_input(path, split_lines=True):
@@ -19,12 +28,14 @@ def __read_input(path, split_lines=True):
 
 
 def read_example_input_by_day(day: int, split_lines=True):
-    path = os.path.join('inputs', 'examples', f'day{day:0>2}.txt')
+    #path = os.path.join('inputs', 'examples', f'day{day:0>2}.txt')
+    path = os.path.join(str(datetime.now().year), f'{day:0>2}', f'example_input.txt')
     return __read_input(path, split_lines=split_lines)
 
 
 def read_input_by_day(day: int, split_lines=True):
-    path = os.path.join('inputs', f'day{day:0>2}.txt')
+    #path = os.path.join('inputs', f'day{day:0>2}.txt')
+    path = os.path.join(str(datetime.now().year), f'{day:0>2}', f'input.txt')
     return __read_input(path, split_lines=split_lines)
 
 
@@ -45,7 +56,9 @@ def read_example_input(day_or_filename: Union[int, str], split_lines=True):
         return read_example_input_by_filename(day_or_filename, split_lines=split_lines)
 
 
-def read_input(day_or_filename: Union[int, str], split_lines=True):
+def read_input(day_or_filename: Union[int, str], split_lines=True, example=False):
+    if example:
+        return read_example_input(day_or_filename, split_lines=split_lines)
     try:
         return read_input_by_day(int(day_or_filename), split_lines=split_lines)
     except ValueError:
